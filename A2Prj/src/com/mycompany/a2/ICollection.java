2c6a45dddfa379b153810a60000fa8af9f12b094
DanielPCurtis <danieldevelopcurtis@gmail.com>
2020-05-28 05:54:38 -0700

    something will happen

--- A2Prj/src/com/mycompany/a2/ICollection.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.a2;
-
-import java.util.Iterator;
-
-public interface ICollection {
-	public void add(GameObject gameObj);
-	public void remove(GameObject gameObj);
-	public IIterator getIterator();
-}
--- A2Prj/src/com/mycompany/a2/IMove.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.a2;
-/**
- * 
- * @author Daniel Curtis and Curtis Botonis
- * 
- */
-public interface IMove {
-	public void move();
-}
